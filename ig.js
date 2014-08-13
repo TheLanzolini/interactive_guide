3a8143d2da0ab0131090819a88b829bff8786c69
Alex Lanzoni <thelanzolini@gmail.com>
2014-08-13 10:34:43 -0400

    added ig-config

--- ig.js
+++ ig.js
@@ -1,51 +1,16 @@
-// This is the interactive guide
-// MIT Licence
-// Github repo:
-// Author: Alex Lanzoni
-// Email: thelanzolini@gmail.com
-// League tag: NA/EUW: TheLanzolini
-
-// |====================================|
-// | SET THESE OPTIONS. LOWER CASE ONLY |
-// |====================================|
-
-var patch = "4.13"
-
-
-var authorName = "thelanzolini";
-var authorWebsite = "twitch.tv/thelanzolini";
-var showRankedInfo = false;
-
-var champion = "caitlyn";
-var summonerSpellOne = "flash";
-var summonerSpellTwo = "ignite";
-var marks = 1;
-var seals = 1;
-var glyphs = 1;
-var quints = 1;
-var offense = [1, 2, 4, 1, 0, 3, 3, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 3, 1, 1];
-var defense = [2, 0, 0, 2, 0, 3, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-var utility = null;
-var tipsNtricks = "Heres a tip, and a Donger behind it!";
-var notes = {
-  note1: "Play Corki"
-  ,note2: "Buy Trinity Force"
-  ,note3: "Do tons of Damage"
-};
-var skillOrder = [0, 2, 0, 1, 0, 3, 0, 2, 0, 2, 3, 2, 2, 1, 1, 3, 1, 1];
-
 // Angular app and controllers; do not touch if you dont know angularjs and js
+console.log(notes);
 var app = angular.module('igApp', []);
 app.controller('igController', ['$scope', function($scope){
   $scope.patch = patch;
   $scope.summonerSpells = {
     "flash": {
       "name": "Flash"
-      ,"imageSrc": "images/summoner/spells/flash.jpg"
+      ,"imageSrc": "http://placehold.it/50x50"
     }
     ,"ignite": {
       "name": "Ignite"
-      ,"imageSrc": "images/summoner/spells/ignite.jpg"
+      ,"imageSrc": "http://placehold.it/50x50"
     }
   };
   $scope.author = {
