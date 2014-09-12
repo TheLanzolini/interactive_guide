09541e9fba191b14bdbc42772653167dd481d381
Alex Lanzoni <thelanzolini@gmail.com>
2014-09-12 14:53:41 -0400

    made the shit some json, will but the rest of the shit on google wsk later

--- ig.js
+++ ig.js
@@ -1,55 +1,15 @@
 // Angular app and controllers; do not touch if you dont know angularjs and js
 var app = angular.module('igApp', []);
-app.controller('igController', ['$scope', function($scope){
-  $scope.patch = patch;
-  $scope.summonerSpells = {
-    "flash": {
-      "name": "Flash"
-      ,"imageSrc": "http://placehold.it/50x50"
-    }
-    ,"ignite": {
-      "name": "Ignite"
-      ,"imageSrc": "http://placehold.it/50x50"
-    }
-  };
-  $scope.author = {
-    name: authorName
-    ,website: authorWebsite
-    ,twitch: authorTwitch
-    ,youtube: authorYouTube
-    ,twitter: authorTwitter
-    ,facebook: authorFacebook
-  };
-  $scope.showRankedInfo = showRankedInfo;
-  console.log($scope.showRankedInfo);
-  $scope.champions = {
-    "caitlyn": {
-      "name": "caitlyn"
-      ,"title": "The Sheriff of Piltover"
-      ,"baseHp": 390
-      ,"baseMp": 255
-      ,"hp5": "4.75"
-      ,"mp5": "6.5"
-      ,"hplvl": 80
-      ,"mplvl": 35
-      ,"champattrs": champattrs
-    }
-    ,"thresh": {
-      "name": "thresh"
-      ,"title": "The Chain Warden"
-      ,"baseHp": 411
-      ,"baseMp": 200
-      ,"hp5": "6"
-      ,"mp5": "5"
-      ,"hplvl": 89
-      ,"mplvl": 44
-      ,"champattrs": champattrs
-    }
-  };
-  $scope.runes = runes;
-  console.log($scope.author);
-  $scope.champion = $scope.champions[champion];
-  $scope.summonerSpellOne = $scope.summonerSpells[summonerSpellOne];
-  $scope.summonerSpellTwo = $scope.summonerSpells[summonerSpellTwo];
-  console.log($scope.champions);
+app.controller('igController', ['$scope', '$http', function($scope, $http){
+
+  //when server is running we can get the info
+  $http.get('ig-condig.json').then(function(data){
+    console.log("ig-config:", data);
+  });
+
+  $http.get('champion-lib.json').then(function(data){
+    console.log("champion-lib:", data);
+  });
+
+
 }]);
